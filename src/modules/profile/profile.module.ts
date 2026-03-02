69f4629cf78a3b7a8b6f49e76d62e8dc7d300941
Aarif <adebayoabdulazeez195@gmail.com>
2026-03-02 16:18:21 +0100

    import JwtModule and DatabaseModule to resolve dependency errors

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ProfileController } from './profile.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProfileService } from './profile.service';
+import { ProfileController } from './profile.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   controllers: [ProfileController],
-  providers: [ProfileService]
+  providers: [ProfileService],
 })
 export class ProfileModule {}
